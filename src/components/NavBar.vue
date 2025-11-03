57b2e39cf36703a601efc0a1b611e516465b055e
abbey eleazer <abbeyeleazer@gmail.com>
2025-11-03 21:44:03 +0000

    nav ui

---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <nav class="w-20 h-screen bg-white flex flex-col justify-between items-center py-4 border-r border-gray-200">
+    <ul class="w-20 h-[50vh] flex flex-col justify-between items-center py-4">
+      <li>
+        <img src="/public/Logomark.png" alt="logo" class="logo" />
+      </li>
+      <li>
+        <img src="/public/home-icon.png" alt="home" class="icon" />
+      </li>
+      <li>
+        <img src="/public/chart-icon.png" alt="chart" class="icon" />
+      </li>
+      <li>
+        <img src="/public/user-icon.png" alt="user" class="icon" />
+      </li>
+
+      <li>
+        <img src="/public/calendar-icon.png" alt="calendar" class="icon" />
+      </li>
+      <li>
+        <img src="/public/flash-icon.png" alt="flash" class="icon" />
+      </li>
+      <li>
+        <img src="/public/notify-icon.png" alt="notify" class="icon" />
+      </li>
+    </ul>
+    <ul class="w-20 h-[15vh] flex flex-col justify-between items-center py-4">
+      <li>
+      <img src="/public/settings-icon.png" alt="settings" class="icon" />
+    </li>
+    <li>
+      <img src="/public/Avatar.png" alt="avatar" class="avatar" />
+    </li>
+  </ul>
+  </nav>
+</template>
+
---
+++ src/views/BoardView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue';
+
+</script>
+
+<template>
+  <main>
+    <aside><NavBar /></aside>
+  </main>
+</template>
+
+
